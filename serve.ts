b9c21b93a3491376ee57894507cbce6d2484266b
TechnoJo4 <technojo4@gmail.com>
2026-01-03 20:38:40 -0500

    debug

--- serve.ts
+++ serve.ts
@@ -39,6 +39,7 @@ const verifyServiceAuth = async (request: Request, lxm: `${string}.${string}.${s
 		throw new AuthRequiredError({ description: `missing or invalid authorization header` });
 
 	const result = await jwtVerifier.verify(authHeader.slice(7), { lxm });
+    console.log(result, authHeader);
 	if (!result.ok)
 		throw new AuthRequiredError({ description: result.error.description });
 
